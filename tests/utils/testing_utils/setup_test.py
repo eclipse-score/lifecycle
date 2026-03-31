a7eb3e1425ae1360d09966a94cead09513eaf6c5
Maciej Kaszynski <maciej.kaszynski@etas.com>
2026-03-31 16:56:29 +0200

    Adding headers

--- tests/utils/testing_utils/setup_test.py
+++ tests/utils/testing_utils/setup_test.py
@@ -1,3 +1,15 @@
+# *******************************************************************************
+# Copyright (c) 2026 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache License Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# SPDX-License-Identifier: Apache-2.0
+# *******************************************************************************
 import pytest
 from pathlib import Path
 import logging
@@ -6,7 +18,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-@pytest.fixture
+@pytest.fixture(autouse=True, scope="function")
 def setup_test(request, target):
     """Setsup the test by uploading all the binaries to the target"""
 
