2bb8d128d668378b9da0a3d29bb31e0c29f8e456
Timo Steuerwald <stt1rlz@bosch.com>
2026-05-29 19:01:16 +0200

    Verify fallback run target has been activated

--- tests/integration/process_complex_rep_failure/control_client_mock.cpp
+++ tests/integration/process_complex_rep_failure/control_client_mock.cpp
@@ -17,42 +17,70 @@
 #include <score/lcm/lifecycle_client.h>
 
 // Given a correct configuration with:
-//   - An initial Run Target named "Startup" containing component named "control_client_mock"
-//   - A Run Target named "run_target_app_does_report_krunning_in_time" containing "control_client_mock" and "component_does_report_krunning_in_time"
-//   - A Run Target named "run_target_app_does_not_report_krunning_in_time" containing "control_client_mock" and "component_does_not_report_krunning_in_time"
+//   - An initial Run Target named "Startup" containing component named
+//   "control_client_mock"
+//   - A Run Target named "run_target_app_does_report_krunning_in_time"
+//   containing "control_client_mock" and
+//   "component_does_report_krunning_in_time"
+//   - A Run Target named "run_target_app_does_not_report_krunning_in_time"
+//   containing "control_client_mock" and
+//   "component_does_not_report_krunning_in_time"
 
-TEST(RecoveryActionComplexRepFailure, ControlClientMock)
-{
-    score::lcm::ControlClient client;
+TEST(RecoveryActionComplexRepFailure, ControlClientMock) {
+  score::lcm::ControlClient client;
 
-    // Establish communication with launch manager
-    TEST_STEP("Report kRunning from ControlClientMock")
-    {
-        auto result = score::lcm::LifecycleClient{}.ReportExecutionState(score::lcm::ExecutionState::kRunning);
-        ASSERT_TRUE(result.has_value()) << "ReportExecutionState() failed: " << result.error().Message();
-    }
-    // Start the run target run_target_app_does_report_krunning_in_time
-    TEST_STEP("Activate RunTarget run_target_app_does_report_krunning_in_time")
-    {
-        score::cpp::stop_token stop_token;
-        auto result = client.ActivateRunTarget("run_target_app_does_report_krunning_in_time").Get(stop_token);
-        EXPECT_TRUE(result.has_value()) << "Activating target run_target_app_does_report_krunning_in_time failed: " << result.error().Message();
-    }
-    // Start the run target run_target_app_does_not_report_krunning_in_time
-    TEST_STEP("Activate RunTarget run_target_app_does_not_report_krunning_in_time")
-    {
-        score::cpp::stop_token stop_token;
-        auto result = client.ActivateRunTarget("run_target_app_does_not_report_krunning_in_time").Get(stop_token);
-        EXPECT_FALSE(result.has_value()) << "Activating target run_target_app_does_not_report_krunning_in_time did not fail as expected.";
-    }
+  ASSERT_TRUE(check_clean({test_end_location, fallback_file}));
 
-    TEST_STEP("Activate RunTarget Off")
-    {
-        client.ActivateRunTarget("Off");
-    }
+  // Establish communication with launch manager
+  TEST_STEP("Report kRunning from ControlClientMock") {
+    auto result = score::lcm::LifecycleClient{}.ReportExecutionState(
+        score::lcm::ExecutionState::kRunning);
+    ASSERT_TRUE(result.has_value())
+        << "ReportExecutionState() failed: " << result.error().Message();
+  }
+  // Start the run target run_target_app_does_report_krunning_in_time
+  TEST_STEP("Activate RunTarget run_target_app_does_report_krunning_in_time") {
+    score::cpp::stop_token stop_token;
+    auto result =
+        client.ActivateRunTarget("run_target_app_does_report_krunning_in_time")
+            .Get(stop_token);
+    EXPECT_TRUE(result.has_value())
+        << "Activating target run_target_app_does_report_krunning_in_time "
+           "failed: "
+        << result.error().Message();
+  }
+  // Limitation: we cannot wait for the transition to fallback to complete
+  sleep(1);
+  // Then, the LM should exhaust retries and trigger the fallback
+  TEST_STEP("Verify fallback run target has not been activated") {
+    EXPECT_FALSE(std::filesystem::exists(fallback_file))
+        << "Fallback run target should has not been activated";
+  }
+  // Start the run target run_target_app_does_not_report_krunning_in_time
+  TEST_STEP(
+      "Activate RunTarget run_target_app_does_not_report_krunning_in_time") {
+    score::cpp::stop_token stop_token;
+    auto result = client
+                      .ActivateRunTarget(
+                          "run_target_app_does_not_report_krunning_in_time")
+                      .Get(stop_token);
+    EXPECT_FALSE(result.has_value())
+        << "Activating target run_target_app_does_not_report_krunning_in_time "
+           "did not fail as expected.";
+  }
+  // Limitation: we cannot wait for the transition to fallback to complete
+  sleep(1);
+  // Then, the LM should exhaust retries and trigger the fallback
+  TEST_STEP("Verify fallback run target was activated") {
+    EXPECT_TRUE(std::filesystem::exists(fallback_file))
+        << "Fallback run target should have been activated";
+  }
+
+  TEST_STEP("Activate RunTarget Off") { client.ActivateRunTarget("Off"); }
 }
 
-int main(int argc, char** argv)
-{
-    return TestRunner(__FILE__, TerminationBehavior::kContinue, TerminationNotification::kTestEnd).RunTests();
+int main(int argc, char **argv) {
+  return TestRunner(__FILE__, TerminationBehavior::kContinue,
+                    TerminationNotification::kTestEnd)
+      .RunTests();
 }
