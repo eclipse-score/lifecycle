09b4c650f50cfb546fa6ebc3bb0e61cf575e9218
LorantLipcsei-ETAS <lil1yok@bosch.com>
2026-06-16 12:19:22 +0100

    Adding process_simple_rep_failure and process_complex_rep_failure integration tests (#238)
    * AZB 97324 Source files for tests 183 and 184
    
    * Fix build
    
    * Remove not needed cmp & its impl
    
    * Rename: Part one, folder only
    
    * Rename: Part two, files and content
    
    * Update test description
    
    * Drop verbose mode as it is nearly not used
    
    * Code review rework: Fix Williams comments
    
    * Move test process cpp to test_helper folder
    
    * Update list of requirements
    
    * Verify fallback run target has been activated
    
    * Tiny documentation changes
    
    * AZB 97324 Addressing PR comments
    
    * AZB 97324 Removing unnecessary code from complex_reporting_process.cpp
    
    * Updating the partially verified lists (#238)
    
    * Removing feat_req__lifecycle__process_failure_react for process_simple_rep_failure
    
    * Removing feat_req__lifecycle__process_failure_react for process_complex_rep_failure
    
    * AZB 97324 Fixing launch manager references in the BUILD files
    
    * Fixing source and BUILD files to reflect the folder structure (#238)
    
    * Fixing Test Helper BUILD file to reflect the folder structure (#238)
    
    * AZB 97324 Fixing the config for process_complex_rep_failure
    
    * Fixing formatting (#238)
    
    ---------
    
    Co-authored-by: Timo Steuerwald <stt1rlz@bosch.com>

---
+++ tests/integration/process_simple_rep_failure/process_simple_reporting.cpp
@@ -0,0 +1,48 @@
+/********************************************************************************
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+#include <chrono>
+#include <thread>
+
+#include <gtest/gtest.h>
+
+#include "tests/utils/test_helper/test_helper.hpp"
+#include <score/mw/lifecycle/lifecycle_client.h>
+
+int g_argc;
+char** g_argv;
+
+TEST(ProcessSimpleRepFailure, ProcessSimpleReporting)
+{
+    // Check arguments
+    TEST_STEP("Check args")
+    {
+        ASSERT_GT(g_argc, 1) << "Wrong number of arguments";
+        ASSERT_FALSE((g_argv[1][0] != '0') && (atoi(g_argv[1])) == 0) << "Argument must be a number";
+    }
+    // Report kRunning with the appropriate delay
+    TEST_STEP("Report kRunning from ProcessSimpleReporting")
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(atoi(g_argv[1])));
+
+        auto result = score::mw::lifecycle::LifecycleClient{}.ReportExecutionState(score::mw::lifecycle::ExecutionState::kRunning);
+        EXPECT_TRUE(result.has_value()) << "ReportExecutionState() failed: " << result.error().Message();
+    }
+}
+
+int main(int argc, char** argv)
+{
+    g_argc = argc;
+    g_argv = argv;
+
+    return TestRunner(__FILE__, TerminationBehavior::kContinue).RunTests();
+}
