qorix-group/inc_lifecycle

hmon: add C++ interface to heartbeat monitor
Add C++ interface to heartbeat monitor.

src/health_monitoring_lib/cpp/include/score/hm/heartbeat/heartbeat_monitor.h
@@ -1,0 +1,84 @@
+/********************************************************************************
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+#ifndef SCORE_HM_HEARTBEAT_HEARTBEAT_MONITOR_H
+#define SCORE_HM_HEARTBEAT_HEARTBEAT_MONITOR_H
+
+#include <score/expected.hpp>
+#include <score/hm/common.h>
+
+namespace score::hm
+{
+// Forward declaration
+class HealthMonitor;
+class HealthMonitorBuilder;
+}  // namespace score::hm
+
+namespace score::hm::heartbeat
+{
+// Forward declaration
+class HeartbeatMonitor;
+
+/// Builder for `HeartbeatMonitor`.
+class HeartbeatMonitorBuilder final : public internal::RustDroppable<HeartbeatMonitorBuilder>
+{
+  public:
+    /// Create a new `HeartbeatMonitorBuilder`.
+    ///
+    /// - `range` - time range between heartbeats.
+    HeartbeatMonitorBuilder(const TimeRange& range);
+
+    HeartbeatMonitorBuilder(const HeartbeatMonitorBuilder&) = delete;
+    HeartbeatMonitorBuilder& operator=(const HeartbeatMonitorBuilder&) = delete;
+
+    HeartbeatMonitorBuilder(HeartbeatMonitorBuilder&&) = default;
+    HeartbeatMonitorBuilder& operator=(HeartbeatMonitorBuilder&&) = delete;
+
+  protected:
+    std::optional<internal::FFIHandle> __drop_by_rust_impl()
+    {
+        return monitor_builder_handle_.drop_by_rust();
+    }
+
+  private:
+    internal::DroppableFFIHandle monitor_builder_handle_;
+
+    // Allow to hide drop_by_rust implementation
+    friend class internal::RustDroppable<HeartbeatMonitorBuilder>;
+
+    // Allow HealthMonitorBuilder to access drop_by_rust implementation
+    friend class ::score::hm::HealthMonitorBuilder;
+};
+
+class HeartbeatMonitor final
+{
+  public:
+    // Delete copy, allow move
+    HeartbeatMonitor(const HeartbeatMonitor&) = delete;
+    HeartbeatMonitor& operator=(const HeartbeatMonitor&) = delete;
+
+    HeartbeatMonitor(HeartbeatMonitor&& other) noexcept = default;
+    HeartbeatMonitor& operator=(HeartbeatMonitor&& other) noexcept = default;
+
+    void heartbeat();
+
+  private:
+    explicit HeartbeatMonitor(internal::FFIHandle monitor_handle);
+
+    // Only `HealthMonitor` is allowed to create `HeartbeatMonitor` instances.
+    friend class score::hm::HealthMonitor;
+    internal::DroppableFFIHandle monitor_handle_;
+};
+
+}  // namespace score::hm::heartbeat
+
+#endif  // SCORE_HM_HEARTBEAT_HEARTBEAT_MONITOR_H

eclipse-score/lifecycle

src/health_monitoring_lib/cpp/include/score/hm/heartbeat/heartbeat_monitor.h
@@ -1,0 +1,84 @@
+/********************************************************************************
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+#ifndef SCORE_HM_HEARTBEAT_HEARTBEAT_MONITOR_H
+#define SCORE_HM_HEARTBEAT_HEARTBEAT_MONITOR_H
+
+#include <score/expected.hpp>
+#include <score/hm/common.h>
+
+namespace score::hm
+{
+// Forward declaration
+class HealthMonitor;
+class HealthMonitorBuilder;
+}  // namespace score::hm
+
+namespace score::hm::heartbeat
+{
+// Forward declaration
+class HeartbeatMonitor;
+
+/// Builder for `HeartbeatMonitor`.
+class HeartbeatMonitorBuilder final : public internal::RustDroppable<HeartbeatMonitorBuilder>
+{
+  public:
+    /// Create a new `HeartbeatMonitorBuilder`.
+    ///
+    /// - `range` - time range between heartbeats.
+    HeartbeatMonitorBuilder(const TimeRange& range);
+
+    HeartbeatMonitorBuilder(const HeartbeatMonitorBuilder&) = delete;
+    HeartbeatMonitorBuilder& operator=(const HeartbeatMonitorBuilder&) = delete;
+
+    HeartbeatMonitorBuilder(HeartbeatMonitorBuilder&&) = default;
+    HeartbeatMonitorBuilder& operator=(HeartbeatMonitorBuilder&&) = delete;
+
+  protected:
+    std::optional<internal::FFIHandle> _drop_by_rust_impl()
+    {
+        return monitor_builder_handle_.drop_by_rust();
+    }
+
+  private:
+    internal::DroppableFFIHandle monitor_builder_handle_;
+
+    // Allow to hide drop_by_rust implementation
+    friend class internal::RustDroppable<HeartbeatMonitorBuilder>;
+
+    // Allow HealthMonitorBuilder to access drop_by_rust implementation
+    friend class ::score::hm::HealthMonitorBuilder;
+};
+
+class HeartbeatMonitor final
+{
+  public:
+    // Delete copy, allow move
+    HeartbeatMonitor(const HeartbeatMonitor&) = delete;
+    HeartbeatMonitor& operator=(const HeartbeatMonitor&) = delete;
+
+    HeartbeatMonitor(HeartbeatMonitor&& other) noexcept = default;
+    HeartbeatMonitor& operator=(HeartbeatMonitor&& other) noexcept = default;
+
+    void heartbeat();
+
+  private:
+    explicit HeartbeatMonitor(internal::FFIHandle monitor_handle);
+
+    // Only `HealthMonitor` is allowed to create `HeartbeatMonitor` instances.
+    friend class score::hm::HealthMonitor;
+    internal::DroppableFFIHandle monitor_handle_;
+};
+
+}  // namespace score::hm::heartbeat
+
+#endif  // SCORE_HM_HEARTBEAT_HEARTBEAT_MONITOR_H