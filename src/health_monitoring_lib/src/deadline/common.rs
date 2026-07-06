qorix-group/inc_lifecycle

DeadlineMonitor: Introduce impl for DeadlineMonitoring
- Deadline API in Rust
- usage fo score_log

src/health_monitoring_lib/src/deadline/common.rs
@@ -1,0 +1,125 @@
+//
+// Copyright (c) 2025 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Apache License Version 2.0 which is available at
+// <https://www.apache.org/licenses/LICENSE-2.0>
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+use std::{
+    ops::Deref,
+    sync::atomic::{AtomicBool, Ordering},
+};
+
+use crate::TimeRange;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub(super) struct StateIndex(usize);
+
+impl StateIndex {
+    pub fn new(index: usize) -> Self {
+        Self(index)
+    }
+}
+
+impl Deref for StateIndex {
+    type Target = usize;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+/// Template for a deadline, managing its range and usage state.
+pub(super) struct DeadlineTemplate {
+    range: TimeRange,
+    is_in_use: AtomicBool,
+    pub assigned_state_index: StateIndex,
+}
+
+impl DeadlineTemplate {
+    pub(super) fn new(range: TimeRange, state_index: StateIndex) -> Self {
+        Self {
+            range,
+            is_in_use: AtomicBool::new(false),
+            assigned_state_index: state_index,
+        }
+    }
+
+    /// Attempts to acquire the deadline for use.
+    /// Returns Some(TimeRange) if successful, None if already in use.
+    pub(super) fn acquire_deadline(&self) -> Option<TimeRange> {
+        if self
+            .is_in_use
+            .compare_exchange(false, true, Ordering::Relaxed, Ordering::Relaxed)
+            .is_ok()
+        {
+            Some(self.range)
+        } else {
+            None
+        }
+    }
+
+    /// Releases the deadline, marking it as not in use.
+    pub(super) fn release_deadline(&self) {
+        self.is_in_use.store(false, Ordering::Relaxed);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::sync::Arc;
+
+    use crate::TimeRange;
+    use core::time::Duration;
+
+    #[test]
+    fn new_and_fields() {
+        let range = TimeRange::new(Duration::from_secs(1), Duration::from_secs(2));
+        let idx = StateIndex::new(7);
+        let tmpl = DeadlineTemplate::new(range, idx);
+        assert_eq!(tmpl.range.min, Duration::from_secs(1));
+        assert_eq!(tmpl.range.max, Duration::from_secs(2));
+        assert_eq!(*tmpl.assigned_state_index, 7);
+        assert!(!tmpl.is_in_use.load(Ordering::Relaxed));
+    }
+
+    #[test]
+    fn acquire_and_release_deadline() {
+        let range = TimeRange::new(Duration::from_secs(3), Duration::from_secs(4));
+        let idx = StateIndex::new(0);
+        let tmpl = Arc::new(DeadlineTemplate::new(range, idx));
+
+        // First acquire should succeed
+        assert_eq!(tmpl.acquire_deadline(), Some(range));
+        // Second acquire should fail
+        assert_eq!(tmpl.acquire_deadline(), None);
+
+        // Release and acquire again
+        tmpl.release_deadline();
+        assert_eq!(tmpl.acquire_deadline(), Some(range));
+    }
+
+    #[test]
+    fn concurrent_acquire() {
+        use std::thread;
+        let range = TimeRange::new(Duration::from_secs(5), Duration::from_secs(6));
+        let idx = StateIndex::new(1);
+        let tmpl = Arc::new(DeadlineTemplate::new(range, idx));
+
+        let tmpl1 = tmpl.clone();
+        let tmpl2 = tmpl.clone();
+        let h1 = thread::spawn(move || tmpl1.acquire_deadline());
+        let h2 = thread::spawn(move || tmpl2.acquire_deadline());
+        let r1 = h1.join().unwrap();
+        let r2 = h2.join().unwrap();
+        // Only one thread should succeed
+        assert!(matches!((r1, r2), (Some(_), None) | (None, Some(_))));
+    }
+}

src/health_monitoring_lib/src/deadline/deadline_state.rs
@@ -1,0 +1,190 @@
+//
+// Copyright (c) 2025 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Apache License Version 2.0 which is available at
+// <https://www.apache.org/licenses/LICENSE-2.0>
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+use std::{
+    fmt::Debug,
+    sync::atomic::{AtomicU64, Ordering},
+};
+
+#[derive(Clone, Copy)]
+pub(super) struct DeadlineStateSnapshot(u64);
+
+// Deadline State layout (u64) = | timestamp: u32 | reserved: 28 bits | finished_too_early: 1 bit | reserved: 1 bit | stopped: 1 bit | running: 1 bit |
+const DEADLINE_STATE_MASK: u64 = 0b0000_1111;
+const DEADLINE_STATE_RUNNING: u64 = 0b0000_0010;
+const DEADLINE_STATE_STOPPED: u64 = 0b0000_0001;
+const DEADLINE_STATE_FINISHED_TOO_EARLY: u64 = 0b0000_1000;
+
+impl DeadlineStateSnapshot {
+    pub(super) fn new(val: u64) -> Self {
+        Self(val)
+    }
+
+    pub(super) fn as_u64(&self) -> u64 {
+        self.0
+    }
+
+    pub(super) fn is_running(&self) -> bool {
+        (self.0 & DEADLINE_STATE_RUNNING) != 0
+    }
+
+    pub(super) fn is_stopped(&self) -> bool {
+        (self.0 & DEADLINE_STATE_STOPPED) != 0
+    }
+
+    pub(super) fn is_underrun(&self) -> bool {
+        (self.0 & DEADLINE_STATE_FINISHED_TOO_EARLY) != 0
+    }
+
+    pub(super) fn timestamp(&self) -> u32 {
+        ((self.0 & !DEADLINE_STATE_MASK) >> u32::BITS) as u32
+    }
+
+    pub(super) fn set_timestamp(&mut self, timestamp: u32) {
+        self.0 = ((timestamp as u64) << u32::BITS) | (self.0 & DEADLINE_STATE_MASK);
+    }
+
+    pub(super) fn set_running(&mut self) {
+        self.0 = self.0 | DEADLINE_STATE_RUNNING;
+    }
+
+    pub(super) fn set_stopped(&mut self) {
+        self.0 = self.0 | DEADLINE_STATE_STOPPED;
+    }
+
+    pub(super) fn set_underrun(&mut self) {
+        self.0 = self.0 | DEADLINE_STATE_FINISHED_TOO_EARLY;
+    }
+}
+
+impl Debug for DeadlineStateSnapshot {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("DeadlineStateSnapshot")
+            .field("timestamp", &self.timestamp())
+            .field("is_running", &self.is_running())
+            .field("is_stopped", &self.is_stopped())
+            .field("is_underrun", &self.is_underrun())
+            .finish()
+    }
+}
+
+impl crate::log::ScoreDebug for DeadlineStateSnapshot {
+    fn fmt(&self, f: crate::log::Writer, spec: &crate::log::FormatSpec) -> Result<(), crate::log::Error> {
+        crate::log::DebugStruct::new(f, spec, "DeadlineStateSnapshot")
+            .field("timestamp", &self.timestamp())
+            .field("is_running", &self.is_running())
+            .field("is_stopped", &self.is_stopped())
+            .field("is_underrun", &self.is_underrun())
+            .finish()
+    }
+}
+
+impl Default for DeadlineStateSnapshot {
+    fn default() -> Self {
+        Self(DEADLINE_STATE_STOPPED)
+    }
+}
+
+pub(super) struct DeadlineState(AtomicU64);
+
+impl DeadlineState {
+    /// Creates a new `DeadlineState` with the initial state set to stopped.
+    pub(super) fn new() -> Self {
+        Self(AtomicU64::new(DEADLINE_STATE_STOPPED))
+    }
+
+    /// Returns a snapshot of the current deadline state.
+    pub(super) fn snapshot(&self) -> DeadlineStateSnapshot {
+        DeadlineStateSnapshot::new(self.0.load(Ordering::Relaxed))
+    }
+
+    /// Updates the deadline state using the provided closure.
+    /// The closure receives the current state snapshot and should return
+    /// an Option with the new state snapshot. If None is returned, the state
+    /// is not updated.
+    pub(super) fn update<F: FnMut(DeadlineStateSnapshot) -> Option<DeadlineStateSnapshot>>(
+        &self,
+        mut f: F,
+    ) -> Result<DeadlineStateSnapshot, DeadlineStateSnapshot> {
+        // Prev values returned
+        self.0
+            .fetch_update(Ordering::Relaxed, Ordering::Relaxed, |prev| {
+                let snapshot = DeadlineStateSnapshot::new(prev);
+                f(snapshot).and_then(|new_snapshot| Some(new_snapshot.as_u64()))
+            })
+            .map(|prev| DeadlineStateSnapshot::new(prev))
+            .map_err(|prev| DeadlineStateSnapshot::new(prev))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn default_state() {
+        let snap = DeadlineStateSnapshot::default();
+        assert!(snap.is_stopped());
+        assert!(!snap.is_running());
+        assert!(!snap.is_underrun());
+        assert_eq!(snap.timestamp(), 0);
+    }
+
+    #[test]
+    fn set_and_get_timestamp() {
+        let mut snap = DeadlineStateSnapshot::default();
+        snap.set_timestamp(0x12345678);
+        assert_eq!(snap.timestamp(), 0x12345678);
+        assert!(!snap.is_running());
+        assert!(snap.is_stopped()); // Default is stopped, running is set as a flag
+        assert!(!snap.is_underrun());
+    }
+
+    #[test]
+    fn set_running() {
+        let mut snap = DeadlineStateSnapshot::default();
+        snap.set_running();
+        assert!(snap.is_running());
+        assert!(snap.is_stopped()); // Default is stopped, running is set as a flag
+        assert!(!snap.is_underrun());
+    }
+
+    #[test]
+    fn set_stopped() {
+        let mut snap = DeadlineStateSnapshot::new(0);
+        snap.set_stopped();
+        assert!(snap.is_stopped());
+        assert!(!snap.is_running());
+        assert!(!snap.is_underrun());
+    }
+
+    #[test]
+    fn set_underrun() {
+        let mut snap = DeadlineStateSnapshot::default();
+        snap.set_underrun();
+        assert!(snap.is_underrun());
+        assert!(!snap.is_running());
+        assert!(snap.is_stopped()); // Default is stopped, running is set as a flag
+    }
+
+    #[test]
+    fn as_u64_and_new() {
+        let mut snap = DeadlineStateSnapshot::default();
+        snap.set_timestamp(42);
+        snap.set_running();
+        let val = snap.as_u64();
+        let snap2 = DeadlineStateSnapshot::new(val);
+        assert_eq!(snap2.timestamp(), 42);
+        assert!(snap2.is_running());
+    }
+}

eclipse-score/lifecycle

src/health_monitoring_lib/src/deadline/common.rs
@@ -1,0 +1,126 @@
+//
+// Copyright (c) 2025 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Apache License Version 2.0 which is available at
+// <https://www.apache.org/licenses/LICENSE-2.0>
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+#![warn(dead_code)]
+
+use core::{
+    ops::Deref,
+    sync::atomic::{AtomicBool, Ordering},
+};
+
+use crate::TimeRange;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub(super) struct StateIndex(usize);
+
+impl StateIndex {
+    pub fn new(index: usize) -> Self {
+        Self(index)
+    }
+}
+
+impl Deref for StateIndex {
+    type Target = usize;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+/// Template for a deadline, managing its range and usage state.
+pub(super) struct DeadlineTemplate {
+    range: TimeRange,
+    is_in_use: AtomicBool,
+    pub assigned_state_index: StateIndex,
+}
+
+impl DeadlineTemplate {
+    pub(super) fn new(range: TimeRange, state_index: StateIndex) -> Self {
+        Self {
+            range,
+            is_in_use: AtomicBool::new(false),
+            assigned_state_index: state_index,
+        }
+    }
+
+    /// Attempts to acquire the deadline for use.
+    /// Returns Some(TimeRange) if successful, None if already in use.
+    pub(super) fn acquire_deadline(&self) -> Option<TimeRange> {
+        if self
+            .is_in_use
+            .compare_exchange(false, true, Ordering::Relaxed, Ordering::Relaxed)
+            .is_ok()
+        {
+            Some(self.range)
+        } else {
+            None
+        }
+    }
+
+    /// Releases the deadline, marking it as not in use.
+    pub(super) fn release_deadline(&self) {
+        self.is_in_use.store(false, Ordering::Relaxed);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::sync::Arc;
+
+    use crate::TimeRange;
+    use core::time::Duration;
+
+    #[test]
+    fn new_and_fields() {
+        let range = TimeRange::new(Duration::from_secs(1), Duration::from_secs(2));
+        let idx = StateIndex::new(7);
+        let tmpl = DeadlineTemplate::new(range, idx);
+        assert_eq!(tmpl.range.min, Duration::from_secs(1));
+        assert_eq!(tmpl.range.max, Duration::from_secs(2));
+        assert_eq!(*tmpl.assigned_state_index, 7);
+        assert!(!tmpl.is_in_use.load(Ordering::Relaxed));
+    }
+
+    #[test]
+    fn acquire_and_release_deadline() {
+        let range = TimeRange::new(Duration::from_secs(3), Duration::from_secs(4));
+        let idx = StateIndex::new(0);
+        let tmpl = Arc::new(DeadlineTemplate::new(range, idx));
+
+        // First acquire should succeed
+        assert_eq!(tmpl.acquire_deadline(), Some(range));
+        // Second acquire should fail
+        assert_eq!(tmpl.acquire_deadline(), None);
+
+        // Release and acquire again
+        tmpl.release_deadline();
+        assert_eq!(tmpl.acquire_deadline(), Some(range));
+    }
+
+    #[test]
+    fn concurrent_acquire() {
+        use std::thread;
+        let range = TimeRange::new(Duration::from_secs(5), Duration::from_secs(6));
+        let idx = StateIndex::new(1);
+        let tmpl = Arc::new(DeadlineTemplate::new(range, idx));
+
+        let tmpl1 = tmpl.clone();
+        let tmpl2 = tmpl.clone();
+        let h1 = thread::spawn(move || tmpl1.acquire_deadline());
+        let h2 = thread::spawn(move || tmpl2.acquire_deadline());
+        let r1 = h1.join().unwrap();
+        let r2 = h2.join().unwrap();
+        // Only one thread should succeed
+        assert!(matches!((r1, r2), (Some(_), None) | (None, Some(_))));
+    }
+}

src/health_monitoring_lib/src/deadline/deadline_state.rs
@@ -1,0 +1,191 @@
+//
+// Copyright (c) 2025 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Apache License Version 2.0 which is available at
+// <https://www.apache.org/licenses/LICENSE-2.0>
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+#![allow(dead_code)]
+
+use core::{
+    fmt::Debug,
+    sync::atomic::{AtomicU64, Ordering},
+};
+
+#[derive(Clone, Copy)]
+pub(super) struct DeadlineStateSnapshot(u64);
+
+// Deadline State layout (u64) = | timestamp: u32 | reserved: 28 bits | finished_too_early: 1 bit | reserved: 1 bit | stopped: 1 bit | running: 1 bit |
+const DEADLINE_STATE_MASK: u64 = 0b0000_1111;
+const DEADLINE_STATE_RUNNING: u64 = 0b0000_0010;
+const DEADLINE_STATE_STOPPED: u64 = 0b0000_0001;
+const DEADLINE_STATE_FINISHED_TOO_EARLY: u64 = 0b0000_1000;
+
+impl DeadlineStateSnapshot {
+    pub(super) fn new(val: u64) -> Self {
+        Self(val)
+    }
+
+    pub(super) fn as_u64(&self) -> u64 {
+        self.0
+    }
+
+    pub(super) fn is_running(&self) -> bool {
+        (self.0 & DEADLINE_STATE_RUNNING) != 0
+    }
+
+    pub(super) fn is_stopped(&self) -> bool {
+        (self.0 & DEADLINE_STATE_STOPPED) != 0
+    }
+
+    pub(super) fn is_underrun(&self) -> bool {
+        (self.0 & DEADLINE_STATE_FINISHED_TOO_EARLY) != 0
+    }
+
+    pub(super) fn timestamp(&self) -> u32 {
+        ((self.0 & !DEADLINE_STATE_MASK) >> u32::BITS) as u32
+    }
+
+    pub(super) fn set_timestamp(&mut self, timestamp: u32) {
+        self.0 = ((timestamp as u64) << u32::BITS) | (self.0 & DEADLINE_STATE_MASK);
+    }
+
+    pub(super) fn set_running(&mut self) {
+        self.0 |= DEADLINE_STATE_RUNNING;
+    }
+
+    pub(super) fn set_stopped(&mut self) {
+        self.0 |= DEADLINE_STATE_STOPPED;
+    }
+
+    pub(super) fn set_underrun(&mut self) {
+        self.0 |= DEADLINE_STATE_FINISHED_TOO_EARLY;
+    }
+}
+
+impl Debug for DeadlineStateSnapshot {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.debug_struct("DeadlineStateSnapshot")
+            .field("timestamp", &self.timestamp())
+            .field("is_running", &self.is_running())
+            .field("is_stopped", &self.is_stopped())
+            .field("is_underrun", &self.is_underrun())
+            .finish()
+    }
+}
+
+impl crate::log::ScoreDebug for DeadlineStateSnapshot {
+    fn fmt(&self, f: crate::log::Writer, spec: &crate::log::FormatSpec) -> Result<(), crate::log::Error> {
+        crate::log::DebugStruct::new(f, spec, "DeadlineStateSnapshot")
+            .field("timestamp", &self.timestamp())
+            .field("is_running", &self.is_running())
+            .field("is_stopped", &self.is_stopped())
+            .field("is_underrun", &self.is_underrun())
+            .finish()
+    }
+}
+
+impl Default for DeadlineStateSnapshot {
+    fn default() -> Self {
+        Self(DEADLINE_STATE_STOPPED)
+    }
+}
+
+pub(super) struct DeadlineState(AtomicU64);
+
+impl DeadlineState {
+    /// Creates a new `DeadlineState` with the initial state set to stopped.
+    pub(super) fn new() -> Self {
+        Self(AtomicU64::new(DEADLINE_STATE_STOPPED))
+    }
+
+    /// Returns a snapshot of the current deadline state.
+    pub(super) fn snapshot(&self) -> DeadlineStateSnapshot {
+        DeadlineStateSnapshot::new(self.0.load(Ordering::Relaxed))
+    }
+
+    /// Updates the deadline state using the provided closure.
+    /// The closure receives the current state snapshot and should return
+    /// an Option with the new state snapshot. If None is returned, the state
+    /// is not updated.
+    pub(super) fn update<F: FnMut(DeadlineStateSnapshot) -> Option<DeadlineStateSnapshot>>(
+        &self,
+        mut f: F,
+    ) -> Result<DeadlineStateSnapshot, DeadlineStateSnapshot> {
+        // Prev values returned
+        self.0
+            .fetch_update(Ordering::Relaxed, Ordering::Relaxed, |prev| {
+                let snapshot = DeadlineStateSnapshot::new(prev);
+                f(snapshot).map(|new_snapshot| new_snapshot.as_u64())
+            })
+            .map(DeadlineStateSnapshot::new)
+            .map_err(DeadlineStateSnapshot::new)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn default_state() {
+        let snap = DeadlineStateSnapshot::default();
+        assert!(snap.is_stopped());
+        assert!(!snap.is_running());
+        assert!(!snap.is_underrun());
+        assert_eq!(snap.timestamp(), 0);
+    }
+
+    #[test]
+    fn set_and_get_timestamp() {
+        let mut snap = DeadlineStateSnapshot::default();
+        snap.set_timestamp(0x12345678);
+        assert_eq!(snap.timestamp(), 0x12345678);
+        assert!(!snap.is_running());
+        assert!(snap.is_stopped()); // Default is stopped, running is set as a flag
+        assert!(!snap.is_underrun());
+    }
+
+    #[test]
+    fn set_running() {
+        let mut snap = DeadlineStateSnapshot::default();
+        snap.set_running();
+        assert!(snap.is_running());
+        assert!(snap.is_stopped()); // Default is stopped, running is set as a flag
+        assert!(!snap.is_underrun());
+    }
+
+    #[test]
+    fn set_stopped() {
+        let mut snap = DeadlineStateSnapshot::new(0);
+        snap.set_stopped();
+        assert!(snap.is_stopped());
+        assert!(!snap.is_running());
+        assert!(!snap.is_underrun());
+    }
+
+    #[test]
+    fn set_underrun() {
+        let mut snap = DeadlineStateSnapshot::default();
+        snap.set_underrun();
+        assert!(snap.is_underrun());
+        assert!(!snap.is_running());
+        assert!(snap.is_stopped()); // Default is stopped, running is set as a flag
+    }
+
+    #[test]
+    fn as_u64_and_new() {
+        let mut snap = DeadlineStateSnapshot::default();
+        snap.set_timestamp(42);
+        snap.set_running();
+        let val = snap.as_u64();
+        let snap2 = DeadlineStateSnapshot::new(val);
+        assert_eq!(snap2.timestamp(), 42);
+        assert!(snap2.is_running());
+    }
+}