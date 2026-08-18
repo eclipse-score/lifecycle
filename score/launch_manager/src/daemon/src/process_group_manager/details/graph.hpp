66a882ef320c739d9b01997a7917d8658bb4d1a5
Daniel Thwaites <daniel.thwaites@etas.com>
2026-08-18 12:12:15 +0100

    Delete old control interface

--- score/launch_manager/src/daemon/src/process_group_manager/details/graph.hpp
+++ score/launch_manager/src/daemon/src/process_group_manager/details/graph.hpp
@@ -26,14 +26,13 @@
 
 #include "score/mw/launch_manager/common/concurrency/mpmc_concurrent_queue.hpp"
 #include "score/mw/launch_manager/common/identifier_hash.hpp"
+#include "score/mw/launch_manager/common/process_group_state_id.hpp"
 #include "score/mw/launch_manager/configuration/config.hpp"
-#include "score/mw/launch_manager/control/control_client_channel.hpp"
 #include "score/mw/launch_manager/osal/semaphore.hpp"
 #include "score/mw/launch_manager/process_group_manager/details/component_event.hpp"
 #include "score/mw/launch_manager/process_group_manager/details/component_of.hpp"
 #include "score/mw/launch_manager/process_group_manager/details/component_task.hpp"
 #include "score/mw/launch_manager/process_group_manager/details/dependency_graph.hpp"
-#include "score/mw/launch_manager/process_group_manager/details/itransition_result_publisher.hpp"
 #include "score/mw/launch_manager/process_group_manager/details/process_handling.hpp"
 #include "score/mw/launch_manager/process_group_manager/details/process_info_node.hpp"
 #include "score/mw/launch_manager/process_group_manager/details/run_target.hpp"
@@ -169,8 +168,7 @@ class Graph final
         uint32_t max_num_nodes,
         GraphConfig& configuration,
         std::shared_ptr<WorkerQueue> job_queue,
-        ProcessHandling process_handling,
-        ITransitionResultPublisher* transition_result_receiver);
+        ProcessHandling process_handling);
 
     /// @brief Destructor to clean up resources used by the Graph object.
     ~Graph();
@@ -235,18 +233,8 @@ class Graph final
     /// getState() returns GraphState::kSuccess.
     IdentifierHash getProcessGroupState();
 
-    /// @return The ProcessInfoNode that has a ControlClientChannel, or nullptr if none exists.
-    const ProcessInfoNode* findControlClient();
-
-    /// @brief Sets the control client that is managing state transitions for this process group.
-    /// @param control_client_id The identifier of the new state manager.
-    void setStateManager(ControlClientID& control_client_id);
-
     /// @brief Update the details for the cancel message to match the current state.
     void updateCancelMessage();
-
-    /// @return Information about the control client managing this process group's state.
-    ControlClientID getStateManager();
 
     /// @return The error code set by the last process that caused an unexpected termination.
     uint32_t getLastExecutionError();
@@ -262,20 +250,6 @@ class Graph final
 
     /// @return The pending state, or an empty hash if no state is pending.
     IdentifierHash getPendingState();
-
-    /// @return The pending event code, or kNotSet if there is none.
-    ControlClientCode getPendingEvent();
-
-    /// @brief Clears the pending event, but only if its current value matches expected.
-    /// @param expected The event code to compare against.
-    void clearPendingEvent(ControlClientCode expected);
-
-    /// @brief Stores a pending event code and notifies the ProcessGroupManager to process it.
-    /// @param event The event code to store.
-    void setPendingEvent(ControlClientCode event);
-
-    /// @return The cancel message prepared when updateCancelMessage() was called.
-    ControlClientMessage& getCancelMessage();
 
     /// @brief A utility function that converts codes to strings for logging purposes
     /// @param state The state to convert
@@ -367,29 +341,11 @@ class Graph final
     /// @brief The interfaces passed to the process nodes to control their OS processes
     ProcessHandling process_handling_;
 
-    /// @brief Class to receive information about the initial state transition result
-    ITransitionResultPublisher* transition_result_receiver_;
-
-    /// @brief The state manager node for this process group
-    ControlClientID last_state_manager_{};
-
-    /// @brief The last execution error set on an unexpected termination
-    uint32_t last_execution_error_{0U};
-
     /// @brief Set the true if this is the MainPG and this is the initial state transition
     bool is_initial_state_transition_{false};
 
     /// @brief The pending state transition, if any
     IdentifierHash pending_state_{""};
-
-    /// @brief Any pending event to report
-    ControlClientCode event_{ControlClientCode::kNotSet};
-
-    /// @brief Reason that tha graph was aborted
-    ControlClientCode abort_code_{ControlClientCode::kNotSet};
-
-    /// @brief The message to send when a transition is cancelled
-    ControlClientMessage cancel_message_{};
 
     /// @brief Constant for Off state.
     const IdentifierHash off_state_{"Off"};
