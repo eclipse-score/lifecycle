e72d26aecac4d3db1c012507ad75107c6284a9c6
Daniel Thwaites <daniel.thwaites@etas.com>
2026-09-03 13:56:44 +0100

    Implement new control interface

--- score/launch_manager/src/daemon/src/process_group_manager/process_group_manager.cpp
+++ score/launch_manager/src/daemon/src/process_group_manager/process_group_manager.cpp
@@ -17,9 +17,11 @@
 #include <algorithm>
 #include <csignal>
 
+#include "score/mw/com/types.h"
 #include "score/mw/launch_manager/common/log.hpp"
 #include "score/mw/launch_manager/process_group_manager/details/process_monitor.hpp"
 #include "score/mw/launch_manager/process_group_manager/process_group_manager.hpp"
+#include "score/mw/lifecycle/details/lm_control_service.h"
 
 namespace score::mw::lifecycle::internal
 {
@@ -148,7 +150,8 @@ bool ProcessGroupManager::initializeProcessGroups()
         configuration_.components_.size() + configuration_.run_targets_.size() + 2,
         configuration_,
         worker_jobs_,
-        ProcessHandling{&process_interface_, process_map_, &file_waiter_, alive_monitor_->getSupervisionFactory()});
+        ProcessHandling{&process_interface_, process_map_, &file_waiter_, alive_monitor_->getSupervisionFactory()},
+        this->offerService());
 
     LM_LOG_DEBUG() << "Process group initialized successfully";
     return true;
@@ -181,6 +184,75 @@ void ProcessGroupManager::createProcessComponentsObjects(std::size_t total_proce
     LM_LOG_DEBUG() << "Creating worker threads...";
     thread_pool_ = std::make_unique<ThreadPool<ComponentTask>>(
         worker_jobs_, static_cast<uint32_t>(ProcessLimits::kNumWorkerThreads), *process_monitor_);
+}
+
+[[nodiscard]] LmControlSkeleton ProcessGroupManager::offerService()
+{
+    const auto instance_specifier =
+        score::mw::com::InstanceSpecifier::Create(std::string{"LaunchManager/StateManager/Instance"});
+    SCORE_LANGUAGE_FUTURECPP_ASSERT_PRD_MESSAGE(
+        instance_specifier.has_value(), instance_specifier.error().Message().data());
+
+    auto instance_result = LmControlSkeleton::Create(instance_specifier.value());
+    SCORE_LANGUAGE_FUTURECPP_ASSERT_PRD_MESSAGE(instance_result.has_value(), instance_result.error().Message().data());
+    auto instance = std::move(instance_result).value();
+
+    const auto activate_run_target_register_result = instance.activate_run_target.RegisterHandler(
+        [this](ActivateRunTargetResponse& response, const ActivateRunTargetRequest& request) {
+            SCORE_LANGUAGE_FUTURECPP_ASSERT_PRD_MESSAGE(
+                request.mode == ActivationMode::kForced, "Only ActivationMode::kForced is implemented");
+
+            IdentifierHash new_state{request.run_target_name.data()};
+
+            if (!graph_->isValidRunTarget(new_state))
+            {
+                response = ActivateRunTargetResponse{
+                    status : RequestStatus::kRejected,
+                    rejection_reason : ExecErrc::kRunTargetDoesntExist
+                };
+                return;
+            }
+
+            if (graph_->getProcessGroupState() == new_state)
+            {
+                response = ActivateRunTargetResponse{
+                    status : RequestStatus::kRejected,
+                    rejection_reason : ExecErrc::kAlreadyInState
+                };
+                return;
+            }
+
+            graph_->startTransition(new_state);
+
+            response = ActivateRunTargetResponse{status : RequestStatus::kAccepted};
+        });
+    SCORE_LANGUAGE_FUTURECPP_ASSERT_PRD_MESSAGE(
+        activate_run_target_register_result.has_value(), activate_run_target_register_result.error().Message().data());
+
+    const auto get_active_run_target_register_result =
+        instance.get_active_run_target.RegisterHandler([this]([[maybe_unused]] GetActiveRunTargetResponse& response) {
+            if (graph_->getState() == GraphState::kInTransition)
+            {
+                response =
+                GetActiveRunTargetResponse{status : QueryStatus::kNotAvailable, run_target : RunTargetName("")};
+            }
+            else
+            {
+                const IdentifierHash state = graph_->getProcessGroupState();
+                const std::lock_guard<std::mutex> lock(IdentifierHash::get_registry_mutex());
+                const std::string& name = IdentifierHash::get_registry()[state.data()];
+
+                response =
+                GetActiveRunTargetResponse{status : QueryStatus::kAvailable, run_target : RunTargetName(name)};
+            }
+        });
+    SCORE_LANGUAGE_FUTURECPP_ASSERT_PRD_MESSAGE(
+        activate_run_target_register_result.has_value(), activate_run_target_register_result.error().Message().data());
+
+    const auto offer_result = instance.OfferService();
+    SCORE_LANGUAGE_FUTURECPP_ASSERT_PRD_MESSAGE(offer_result.has_value(), offer_result.error().Message().data());
+
+    return instance;
 }
 
 bool ProcessGroupManager::run()
