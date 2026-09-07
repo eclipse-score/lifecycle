eclipse-score/inc_lifecycle

Reorganise supervision control client

score/launch_manager/src/daemon/src/alive_monitor/details/ifexm/supervision_handle.hpp
@@ -13,11 +13,11 @@
 #ifndef SUPERVISION_HANDLE_HPP_INCLUDED
 #define SUPERVISION_HANDLE_HPP_INCLUDED
 
+#include "score/mw/launch_manager/alive_monitor/details/ifexm/supervision_event.hpp"
+#include "score/mw/launch_manager/alive_monitor/isupervision_event_publisher.hpp"
 #include "score/mw/launch_manager/common/alive_interface_path.hpp"
 #include "score/mw/launch_manager/common/identifier_hash.hpp"
 #include "score/mw/launch_manager/common/log.hpp"
-#include "score/mw/launch_manager/supervision_control_client/isupervision_event_publisher.hpp"
-#include "score/mw/launch_manager/supervision_control_client/supervision_event.hpp"
 
 #include <ctime>
 

score/launch_manager/src/daemon/src/process_group_manager/details/graph_UT.cpp
@@ -20,11 +20,11 @@
 #include <utility>
 #include <vector>
 
+#include "score/mw/launch_manager/alive_monitor/mock_supervision_event_publisher.hpp"
+#include "score/mw/launch_manager/alive_monitor/mock_supervision_factory.hpp"
 #include "score/mw/launch_manager/configuration/config.hpp"
 #include "score/mw/launch_manager/process_group_manager/details/graph.hpp"
 #include "score/mw/launch_manager/process_group_manager/mock_iprocess.hpp"
-#include "score/mw/launch_manager/supervision_control_client/mock_supervision_event_publisher.hpp"
-#include "score/mw/launch_manager/supervision_control_client/mock_supervision_factory.hpp"
 
 namespace score::mw::lifecycle::internal
 {

score/launch_manager/src/daemon/src/process_group_manager/details/process_group_manager_UT.cpp
@@ -14,9 +14,9 @@
 #include "score/mw/launch_manager/process_group_manager/process_group_manager.hpp"
 
 #include "score/mw/launch_manager/alive_monitor/mock_alive_monitor.hpp"
+#include "score/mw/launch_manager/alive_monitor/mock_supervision_event_publisher.hpp"
+#include "score/mw/launch_manager/alive_monitor/mock_supervision_factory.hpp"
 #include "score/mw/launch_manager/recovery_client/mock_irecovery_client.h"
-#include "score/mw/launch_manager/supervision_control_client/mock_supervision_event_publisher.hpp"
-#include "score/mw/launch_manager/supervision_control_client/mock_supervision_factory.hpp"
 #include "score/mw/launch_manager/watchdog/mock_IWatchdogIf.hpp"
 
 #include <gmock/gmock.h>

eclipse-score/lifecycle

score/launch_manager/src/daemon/src/alive_monitor/details/ifexm/supervision_handle.hpp
@@ -13,11 +13,11 @@
 #ifndef SUPERVISION_HANDLE_HPP_INCLUDED
 #define SUPERVISION_HANDLE_HPP_INCLUDED
 
+#include "score/mw/launch_manager/alive_monitor/details/ifexm/supervision_event.hpp"
+#include "score/mw/launch_manager/alive_monitor/ialive_supervision_handle.hpp"
 #include "score/mw/launch_manager/common/alive_interface_path.hpp"
 #include "score/mw/launch_manager/common/identifier_hash.hpp"
 #include "score/mw/launch_manager/common/log.hpp"
-#include "score/mw/launch_manager/supervision_control_client/ialive_supervision_handle.hpp"
-#include "score/mw/launch_manager/supervision_control_client/supervision_event.hpp"
 
 #include <ctime>
 

score/launch_manager/src/daemon/src/process_group_manager/details/graph_UT.cpp
@@ -20,11 +20,11 @@
 #include <utility>
 #include <vector>
 
+#include "score/mw/launch_manager/alive_monitor/mock_alive_supervision_handle.hpp"
+#include "score/mw/launch_manager/alive_monitor/mock_supervision_factory.hpp"
 #include "score/mw/launch_manager/configuration/config.hpp"
 #include "score/mw/launch_manager/process_group_manager/details/graph.hpp"
 #include "score/mw/launch_manager/process_group_manager/mock_iprocess.hpp"
-#include "score/mw/launch_manager/supervision_control_client/mock_alive_supervision_handle.hpp"
-#include "score/mw/launch_manager/supervision_control_client/mock_supervision_factory.hpp"
 
 namespace score::mw::lifecycle::internal
 {
@@ -527,9 +527,8 @@ TEST_F(GraphHandleComponentEventTest, failedFirstDuringTransition)
 
     // Fail the first job
     const auto first_job = job_queue_->pop();
-    graph_->handleComponentEvent(
-        ActivationFailed{
-            first_job->value().component.get().getIdentifier(), IComponent::ComponentError::kErrorBeforeReady});
+    graph_->handleComponentEvent(ActivationFailed{
+        first_job->value().component.get().getIdentifier(), IComponent::ComponentError::kErrorBeforeReady});
 
     const auto second_job = job_queue_->pop();
 
@@ -546,9 +545,8 @@ TEST_F(GraphHandleComponentEventTest, failureFollowedBySuccessFails)
 
     // Fail the first job
     const auto first_job = job_queue_->pop();
-    graph_->handleComponentEvent(
-        ActivationFailed{
-            first_job->value().component.get().getIdentifier(), IComponent::ComponentError::kErrorBeforeReady});
+    graph_->handleComponentEvent(ActivationFailed{
+        first_job->value().component.get().getIdentifier(), IComponent::ComponentError::kErrorBeforeReady});
 
     const auto second_job = job_queue_->pop();
     executeJobSuccessfully(second_job->value());

score/launch_manager/src/daemon/src/process_group_manager/details/process_group_manager_UT.cpp
@@ -14,9 +14,9 @@
 #include "score/mw/launch_manager/process_group_manager/process_group_manager.hpp"
 
 #include "score/mw/launch_manager/alive_monitor/mock_alive_monitor.hpp"
+#include "score/mw/launch_manager/alive_monitor/mock_alive_supervision_handle.hpp"
+#include "score/mw/launch_manager/alive_monitor/mock_supervision_factory.hpp"
 #include "score/mw/launch_manager/recovery_client/mock_irecovery_client.h"
-#include "score/mw/launch_manager/supervision_control_client/mock_alive_supervision_handle.hpp"
-#include "score/mw/launch_manager/supervision_control_client/mock_supervision_factory.hpp"
 #include "score/mw/launch_manager/watchdog/mock_IWatchdogIf.hpp"
 
 #include <gmock/gmock.h>